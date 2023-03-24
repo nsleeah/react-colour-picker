777100c107d250ff8134f10b3fa3734d8cba3b4a
Leahnicoles <lnsimonn@hotmail.com>
2023-03-24 22:26:41 +0000

    import use effect to implement changes

--- src/containers/ColourPickerContainer.js
+++ src/containers/ColourPickerContainer.js
@@ -1,4 +1,4 @@
-import { useState } from "react"; //More Efficient Than React
+import { useState, useEffect } from "react"; //More Efficient Than React
 import Slider from "../components/Slider";
 import RGBDisplay from "../components/RGBDisplay";
 
@@ -8,8 +8,18 @@ const ColourPickerContainer = () => {
     const [red, setRed] = useState(50); //50% Position on Slider.
     const [green, setGreen] = useState(50);
     const [blue, setBlue] = useState(50);
-    const [rgb, setRgb] = useState("rgb(128, 128, 128");
-
+    const [rgb, setRgb] = useState("rgb(128, 128, 128)"); //RGB Between 0-255
+    
+     //2 Arguments
+    //Convert percentages to values between 255.
+    useEffect(() => {
+        const red255 = Math.ceil(red * 2.55); //Value * 255 Then Math.Ceil To Round Up.
+        const green255 = Math.ceil(green * 2.55);
+        const blue255 = Math.ceil(blue * 2.55);
+        setRgb(`rgb(${red255}, ${green255}, ${blue255})
+        `)
+    });
+    
     return (
         <>
             <Slider

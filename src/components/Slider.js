199277508eb5ceb47e837491abeeb2414c827ada
Leahnicoles <lnsimonn@hotmail.com>
2023-03-24 22:05:10 +0000

    declare what we want the slider to display and add labels

---
+++ src/components/Slider.js
@@ -0,0 +1,21 @@
+//React Component Taking in 3 Props.
+const Slider = ({colourName, rgbValue, onSliderMove}) => //Destructuring 
+{ 
+
+    return (
+        <div className="slider">
+            <label htmlFor={colourName}> 
+                {colourName}
+            </label>
+            <input 
+                type="range"
+                id={colourName}
+                onInput={}
+            />
+             </div>
+    )
+}
+
+export default Slider;
+//Label Important for Accesibility, Labels an Input.
+//Label Matches the For with the ID so needs to be the same.
